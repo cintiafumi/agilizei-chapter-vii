f9f9bb78224bcb4ded17b5fbcdda5072662ac4c0
Cintia Yamamoto <cintiafumi@gmail.com>
2021-11-04 22:08:57 -0300

    ✅  tests: ping healthcheck

---
+++ cypress/integration/ping.spec.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+import req from '../support/api/request'
+import assertions from '../support/api/assertions'
+
+describe('Ping', () => {
+  it('should GET HealthCheck', () => {
+    req.getPing()
+      .then((response) => assertions.shouldHaveStatus(response, 201))
+  });
+});
